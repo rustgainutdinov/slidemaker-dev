81e78bc53fb92a92e5b8f1476641af81185ee2bd
Rustam <rustamikc@gmail.com>
2021-01-10 01:19:48 +0300

    удалил эслинткэш

--- src/components/Editor/menu/toolsPanel.tsx
+++ src/components/Editor/menu/toolsPanel.tsx
@@ -1,39 +1,36 @@
-
 import {Control} from "./control";
 import {UpdateControlElements} from "./control/updateControlElements";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { redoState, undoState } from "../../../store/Reducer/editor";
-import { PresentationEl } from "./presentationName";
-import { SlideBackgroundEl } from "./slideBackground";
-import { DeletingControl } from "./control/deletingControl";
-import { PresentationEl } from "./presentationName";
-import { SlideBackgroundEl } from "./slideBackground";
+import {useDispatch} from "react-redux";
+import {redoState, undoState} from "../../../store/Reducer/editor";
+import {PresentationEl} from "./presentationName";
+import {SlideBackgroundEl} from "./slideBackground";
+import {DeletingControl} from "./control/deletingControl";
 
 export const ToolsPanel = () => {
     const dispatch = useDispatch();
 
     return (
         <div className="tools_panel">
-            <h1 className="title"><PresentationEl /></h1>
+            <h1 className="title"><PresentationEl/></h1>
             <div className="tools">
                 <li><a href="#" className="tools_button">Файл</a></li>
                 <li><a href="#" className="tools_button" onClick={() => dispatch(undoState())}>UNDO</a></li>
                 <li><a href="#" className="tools_button" onClick={() => dispatch(redoState())}>REDO</a></li>
                 <li><a href="#" className="tools_button">Вставка</a>
                     <ul className="dropdown">
-                        <Control />
+                        <Control/>
                     </ul>
                 </li>
-                <li><DeletingControl /></li>
+                <li><DeletingControl/></li>
                 <li><a href="#" className="tools_button">Фон</a>
                     <ul className="dropdown">
-                        <SlideBackgroundEl />
+                        <SlideBackgroundEl/>
                     </ul>
                 </li>
                 <li>
                     <a href="#edit_params" className="tools_button">Изменить</a>
-                    <UpdateControlElements />
+                    <UpdateControlElements/>
                 </li>
             </div>
         </div>
