4a4bbbd4a4a9c9ec6dcdcbb016ce095748b660c6
Aleksandrgithub <akksdayz@gmail.com>
2021-01-10 12:06:18 -0800

    color palette add

--- src/components/Editor/menu/textTools/textColor.tsx
+++ src/components/Editor/menu/textTools/textColor.tsx
@@ -27,9 +27,41 @@ export const ColorEl = ({ presentEditor, currentTextContainer }: updateTextConta
         <li>
             <input className="colorPalette" type="color" list="colorList" onInput={handleSelectTextColor} />
             <datalist id="colorList">
-                <option value="#ff0000" label="Красный" />
-                <option value="#008000" label="Зелёный" />
-                <option value="#0000ff" label="Синий" />
+                <option value="#dcdcdc" />
+                <option value="#c0c0c0" />
+                <option value="#a9a9a9" />
+                <option value="#696969" />
+                <option value="#000000" />
+                <option value="#deb887" />
+                <option value="#f4a460" />
+                <option value="#b8860b" />
+                <option value="#b8860b" />
+                <option value="#8b4513" />
+                <option value="#ffa07a" />
+                <option value="#fa8072" />
+                <option value="#f08080" />
+                <option value="#dc143c" />
+                <option value="#ff0000" />
+                <option value="#00bfff" />
+                <option value="#1e90ff" />
+                <option value="#7b68ee" />
+                <option value="#0000ff" />
+                <option value="#00008b" />
+                <option value="#00ff7f" />
+                <option value="#3cb371" />
+                <option value="#228b22" />
+                <option value="#008000" />
+                <option value="#006400" />
+                <option value="#ee82ee" />
+                <option value="#da70d6" />
+                <option value="#c71585" />
+                <option value="#8b008b" />
+                <option value="#4b0082" />
+                <option value="#ff7f50" />
+                <option value="#ff6347" />
+                <option value="#ff4500" />
+                <option value="#ff8c00" />
+                <option value="#ffa500" />
             </datalist>
         </li >
     )
