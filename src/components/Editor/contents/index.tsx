2e4d3e1008b4b6342f944e0a4026f16833b21e60
algolGuru <gleberoshkin1803@gmail.com>
2021-01-10 20:54:07 +0300

    переделал background set

--- src/components/Editor/contents/index.tsx
+++ src/components/Editor/contents/index.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
-import {CircleEl} from './circle';
-import {RootState} from "../../../store/Reducer";
+import { CircleEl } from './circle';
+import { RootState } from "../../../store/Reducer";
 import Content from "../../../model/slide/content/Content";
-import {isCircle} from "../../../methods/typeGuardMethods/isCircle";
-import {useDispatch, useSelector} from "react-redux";
-import {addState} from "../../../store/Reducer/editor";
+import { isCircle } from "../../../methods/typeGuardMethods/isCircle";
+import { useDispatch, useSelector } from "react-redux";
+import { addState } from "../../../store/Reducer/editor";
 import Editor from "../../../model/Editor";
-import {isRectangle} from "../../../methods/typeGuardMethods/isRectangle";
-import {RectangleEl} from "./rectangles";
-import {isTextContainer} from "../../../methods/typeGuardMethods/isTextContainer";
-import {TextContainerEl} from "./textContainers";
+import { isRectangle } from "../../../methods/typeGuardMethods/isRectangle";
+import { RectangleEl } from "./rectangles";
+import { isTextContainer } from "../../../methods/typeGuardMethods/isTextContainer";
+import { TextContainerEl } from "./textContainers";
 import changeCurrentContent from "../../../methods/updateContent/changeCurrentContent";
-import {getIteratedCurrSlideContentList} from "../../../methods/core/getIteratedCurrSlideContentList";
-import {isImage} from "../../../methods/typeGuardMethods/isImage";
-import {ImageEl} from './image';
+import { getIteratedCurrSlideContentList } from "../../../methods/core/getIteratedCurrSlideContentList";
+import { isImage } from "../../../methods/typeGuardMethods/isImage";
+import { ImageEl } from './image';
 
 export const Contents: React.FC = () => {
     const editor: Editor = useSelector((state: RootState) => state.editorReducer.present);
@@ -26,32 +26,29 @@ export const Contents: React.FC = () => {
         const onClickCallback = () => changeCurrentContentAction(content.uuid);
         if (isCircle(content)) {
             return <CircleEl circle={content} onClick={onClickCallback.bind(content)} isCurrent={true}
-                             key={content.uuid}/>
+                key={content.uuid} />
         }
         if (isRectangle(content)) {
             return <RectangleEl rectangle={content} isCurrent={true} key={content.uuid}
-                                onClick={onClickCallback.bind(content)}/>
+                onClick={onClickCallback.bind(content)} />
         }
         if (isTextContainer(content)) {
-            return <TextContainerEl textContainer={content} isCurrent={true} onClick={onClickCallback.bind(content)} key={content.uuid}/>
+            return <TextContainerEl textContainer={content} isCurrent={true} onClick={onClickCallback.bind(content)} key={content.uuid} />
         }
         if (isImage(content)) {
-            return <ImageEl image={content} isCurrent={true} onClick={onClickCallback.bind(content)} key={content.uuid}/>
+            return <ImageEl image={content} isCurrent={true} onClick={onClickCallback.bind(content)} key={content.uuid} />
         }
     };
-    
+
     const drawBackground = () => {
-        if (editor.currentSlide?.background == '') {
-            return (
-                <image href={editor.currentSlide?.backgroundImage} height="100%" width="100%"/>
-            )
-        } else {
-            return(
+        return (
+            <>
                 <rect width={'100%'} height={'100%'} fill={editor.currentSlide?.background} />
-            )
-        }
+                <image href={editor.currentSlide?.backgroundImage} height="100%" width="100%" />
+            </>
+        )
     }
-    
+
     return (
         <svg width={1000} height="75vh" id={"currentSlideContent"}>
             {drawBackground()}
--- src/methods/updateContent/updateSlideBackground.ts
+++ src/methods/updateContent/updateSlideBackground.ts
@@ -1,15 +1,33 @@
 import Editor from "../../model/Editor";
 
-export function updateSlideBackground(editor: Editor, background: string): Editor {
+export function updateSlideBackground(editor: Editor, background: string, backgroundImage: string): Editor {
     if (!editor.currentSlide) {
         return editor;
     }
-    return {
-        ...editor,
-        currentSlide: {
-            ...editor.currentSlide,
-            backgroundImage: '',
-            background: background
-        }
-    };
+    if (background == '') {
+        return {
+            ...editor,
+            currentSlide: {
+                ...editor.currentSlide,
+                backgroundImage: backgroundImage
+            }
+        };
+    } else if (backgroundImage == '') {
+        return {
+            ...editor,
+            currentSlide: {
+                ...editor.currentSlide,
+                background: background,
+            }
+        };
+    } else {
+        return {
+            ...editor,
+            currentSlide: {
+                ...editor.currentSlide,
+                background: background,
+                backgroundImage: backgroundImage
+            }
+        };
+    }
 }
