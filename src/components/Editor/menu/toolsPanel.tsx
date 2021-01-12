cd1411a0c5921dc694c8431c533ad097d21cbd0d
Rustam <rustamikc@gmail.com>
2021-01-12 17:57:08 +0300

    анду реду красиво

--- src/components/Editor/menu/toolsPanel.tsx
+++ src/components/Editor/menu/toolsPanel.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { redoState, undoState } from "../../../store/Reducer/editor";
-import { SlideBackgroundEl } from "./slideBackground";
-import { DeletingControl } from "./control/deletingControl";
-import { ControlEl } from "./control";
-import { ShapeTools } from "./shapeTools/shapeTools";
-import { TextTools } from "./textTools/textTools";
+import {useDispatch} from "react-redux";
+import {redoState, undoState} from "../../../store/Reducer/editor";
+import {SlideBackgroundEl} from "./slideBackground";
+import {DeletingControl} from "./control/deletingControl";
+import {ControlEl} from "./control";
+import {ShapeTools} from "./shapeTools/shapeTools";
+import {TextTools} from "./textTools/textTools";
 import './toolsPanel.css'
-import { PresentationEl } from "./presentationName";
-import { Export } from "./file/export";
-import { Import } from "./file/import";
-import { Images } from "./imageTools/imageTools";
+import {PresentationEl} from "./presentationName";
+import {Export} from "./file/export";
+import {Import} from "./file/import";
+import {Images} from "./imageTools/imageTools";
+import {UndoIcon} from "./icons/undo";
+import {RedoIcon} from "./icons/redo";
 
 
 export const ToolsPanel = () => {
@@ -18,22 +20,30 @@ export const ToolsPanel = () => {
 
     return (
         <div className="tools_panel">
-            <h1 className="title"><PresentationEl /></h1>
+            <h1 className="title"><PresentationEl/></h1>
             <div className="tools">
                 <div className="main_tools">
-                    <Import />
-                    <Export />
-                    <li><a href="#" className="tools_button" onClick={() => dispatch(undoState())}>Undo</a></li>
-                    <li><a href="#" className="tools_button" onClick={() => dispatch(redoState())}>Redo</a></li>
-                    <li><DeletingControl /></li>
-                    <li><a href="#" className="tools_button">Фон</a>
-                        <SlideBackgroundEl />
+                    <Import/>
+                    <Export/>
+                    <li>
+                        <a href="#" className="tools_button tools_icon" onClick={() => dispatch(undoState())}>
+                            <UndoIcon/>
+                        </a>
                     </li>
-                    <ControlEl />
+                    <li>
+                        <a href="#" className="tools_button tools_icon" onClick={() => dispatch(redoState())}>
+                            <RedoIcon/>
+                        </a>
+                    </li>
+                    <li><DeletingControl/></li>
+                    <li><a href="#" className="tools_button">Фон</a>
+                        <SlideBackgroundEl/>
+                    </li>
+                    <ControlEl/>
                 </div>
                 <Images/>
-                <ShapeTools />
-                <TextTools />
+                <ShapeTools/>
+                <TextTools/>
             </div>
         </div>
 
