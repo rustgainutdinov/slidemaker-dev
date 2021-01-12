ef04ea072ff356f1a6b2342b6d9ef2817ef9f267
gleb.eroshkin <gleb.eroshkin@travelline.ru>
2021-01-12 17:03:48 +0300

    бг сетер вновь

--- src/components/Editor/menu/slideBackground.tsx
+++ src/components/Editor/menu/slideBackground.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateSlideBackground } from "../../../methods/updateContent/updateSlideBackground";
 import { updateSlideBackgroundImage } from "../../../methods/updateContent/updateSlideBackgroundImage";
